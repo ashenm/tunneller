465ba2ca629aa52c9515dd50e5398bdedad682da
Ashen Gunaratne <mail@ashenm.ml>
2019-03-14 10:21:58 +0000

    Tidy
    - Uniform directory structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@
 set -e
 
 # clinch permissions
-find ssh -type d -exec chmod 755 {} \;
-find ssh/etc -type f -exec chmod 644 {} \;
-find ssh/sbin -type f -exec chmod 755 {} \;
+find tunneller -type d -exec chmod 755 {} \;
+find tunneller/etc -type f -exec chmod 644 {} \;
+find tunneller/sbin -type f -exec chmod 755 {} \;
 
 # build docker image
 docker build --no-cache --tag "${TRAVIS_REPO_SLUG:-ashenm/tunneller}:${TRAVIS_BRANCH:-latest-alpha}" .
